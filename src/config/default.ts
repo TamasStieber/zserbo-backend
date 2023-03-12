35f35ac111339b86b962057fcf1cb8838fc4715a
Tamas Stieber <stiebertamas@gmail.com>
2023-03-13 00:24:56 +0100

    fix(default.ts): Correct mongo atlas connection string

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,12 +1,17 @@
-import * as dotenv from 'dotenv'
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 
-export const VALID_USERNAME = process.env.VALID_USERNAME || '';
-export const VALID_PASSWORD = process.env.VALID_PASSWORD || '';
+export const VALID_USERNAME = process.env.VALID_USERNAME || "";
+export const VALID_PASSWORD = process.env.VALID_PASSWORD || "";
 
-export const MONGO_PATH = process.env.MONGO_PATH || '';
+export const MONGO_USER = process.env.MONGO_USER || "";
+export const MONGO_PW = process.env.MONGO_PW || "";
+export const MONGO_URI = process.env.MONGO_URI || "";
 
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || '';
+export const MONGO_PATH =
+  `mongodb+srv://${MONGO_USER}:${MONGO_PW}@${MONGO_URI}` || "";
+
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "";
